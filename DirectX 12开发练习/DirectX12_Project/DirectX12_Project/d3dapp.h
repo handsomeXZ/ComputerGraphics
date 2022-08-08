2c940ce71f8ac848bda52564bcd00bccf168ea1b
jtj <9900647+vcafilm@user.noreply.gitee.com>
2022-08-08 20:51:58 +0800

    update bool InitDirect3D12()

--- DirectX 12开发练习/DirectX12_Project/DirectX12_Project/d3dapp.h
+++ DirectX 12开发练习/DirectX12_Project/DirectX12_Project/d3dapp.h
@@ -23,6 +23,7 @@ protected:
 
     bool InitMainWindow();
     bool InitDirect3D12();
+    bool CreateSwapChain();
 
 protected:
     static D3DApp* mApp;
@@ -34,7 +35,26 @@ protected:
     Microsoft::WRL::ComPtr<ID3D12Device> md3dDevice;
     Microsoft::WRL::ComPtr<ID3D12Fence> mFence;
 
-    UINT16 mRtvDescriptorSize;
+    UINT mRtvDescriptorSize;
+    UINT mDsvDescriptorSize;
+    UINT mCbvUavDescriptorSize;
+
+    Microsoft::WRL::ComPtr<ID3D12CommandQueue> mCommandQueue;
+    Microsoft::WRL::ComPtr<ID3D12CommandAllocator> mCommandAllocator;
+    Microsoft::WRL::ComPtr<ID3D12CommandList> mCommandList;
+
+    // ½»»»Á´
+    DXGI_FORMAT mBackBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+    UINT SwapChainBufferCount = 2;
+    UINT m4xMsaaQualityLevels;
+    UINT mCurrentBuffer = 0;
+    Microsoft::WRL::ComPtr<IDXGISwapChain> mSwapChain;
+    D3D12_CPU_DESCRIPTOR_HANDLE CurrentBackBufferView();
+    
+
+    // ÃèÊö·û¶Ñ
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> mRtvHeap;
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> mDsvHeap;
 
 
     // Derived class should set these in derived constructor to customize starting values.
